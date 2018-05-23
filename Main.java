91305356f7936ec4ead3918b73446a51d1a9d7bb
edgar <evalderrama862@gmail.com>
2018-05-22 22:37:26 -0400

    CPU with threads and sync

--- Main.java
+++ Main.java
@@ -6,14 +6,25 @@ import static java.lang.Thread.sleep;
 class CubbyHole{
     private RedBlackTree<Program> tree = new RedBlackTree<>();
 
-    private int quantum = 0;
+    private int quantum = 2;
 
     public synchronized void new_proccess(ArrayList<Program> program_list){
         Program program = program_list.remove(0);
         tree.insert(program);
+
+        notifyAll();
     }
 
     public synchronized Program get_process(){
+
+        while (this.tree.isEmpty()){
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
         Program program = tree.pop_most_left().get_key();
 
         if (program.isCompleted(quantum)){
@@ -22,13 +33,15 @@ class CubbyHole{
                 sleep(quantum);
             } catch (InterruptedException e) { }
         } else {
+            // If program does not finish. Add time and return to tree.
             program.addExecutedTime(quantum);
-            tree.insert(program);
 
             // Here we wait quantum.
             try {
                 sleep(quantum);
             } catch (InterruptedException e) { }
+
+            tree.insert(program);
         }
 
         return program;
@@ -63,6 +76,13 @@ class CPU extends Thread {
     public void run(){
         while(true){
             this.cubbyHole.get_process();
+
+            try {
+                sleep(100);
+                System.out.print("hueheuheuheuehueheuheu\n");
+            } catch (InterruptedException e) {
+                System.out.print("not so much huehue\n");
+            }
         }
     }
 }
@@ -80,6 +100,9 @@ public class Main {
 
         Producer p1 = new Producer(cubbyHole, program_list);
 
+        CPU cpu1 = new CPU(cubbyHole);
+
         p1.run();
+        cpu1.run();
     }
 }
